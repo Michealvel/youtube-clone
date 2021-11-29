d374c301a548896e746f43a3a18daf15a3705cae
Peter Baah <petermv.baah@gmail.com>
2021-11-28 19:31:25 -0500

    added user.js on the models folder

--- models/product.js
+++ models/product.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const joi = require ("joi");
+const Joi = require("joi");
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true, minlength: 2, maxlength: 255 },
   description: { type: String, required: true },
@@ -8,6 +10,22 @@ const productSchema = new mongoose.Schema({
 });
 
 const Product = mongoose.model("Product", productSchema);
+
+function validateProduct (product) {
+  const schema = Joi.object({
+    name: Joi.string() . min(2).max(50). required(),
+    description: Joi.string(). required (),
+    category: Joi.string().min(5).max(50).required(),
+    price: Joi.number() .required()
+  });
+  return schema.validate(product);
+
+}
+
+exports.Product = Product;
+exports.validate = validateProduct;
+exports.productSchema = productSchema;
+
 
 module.exports = Product;
 
